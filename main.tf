bd133c6a8be99ce88b4a32bace65b1be4a23596c
Karishma Chawla <kachawla@microsoft.com>
2024-04-17 22:06:48 -0700

    Add main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0"
+    }
+  }
+}
+
+resource "random_id" "resource" {
+  keepers = {
+    # Generate a new id for every unique resource id
+    resource_id = var.context.resource.id
+  }
+
+  byte_length = 8
+}
+
+module "redis" {
+  source  = "Azure/postgresql/azurerm"
+
+  resource_group_name = var.context.azure.resourceGroup.name
+  location            = var.location
+  server_name    = "pg-${random_id.resource.hex}"
+  administrator_login           = var.username
+  administrator_password        = var.password
+}
