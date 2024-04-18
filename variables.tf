05cf76305a5ff8a3eb35749acc9ef750d5b83c6f
Karishma Chawla <kachawla@microsoft.com>
2024-04-17 22:09:55 -0700

    Add variables.tf

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "location" {
+  type        = string
+  description = "Azure location."
+}
+
+variable "username" {
+  type        = string
+  description = "Username for the Postgres Server."
+}
+
+variable "administrator_password" {
+  type        = string
+  description = "Password associated with the username for the Postgres Server."
+  sensitive   = true
+}
+
+variable "context" {
+  description = "This variable contains Radius recipe context."
+
+  type = any
+}
